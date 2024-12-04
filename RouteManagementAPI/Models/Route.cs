c21b429286ca4fa82d54410cd9d81ae4390fe72d
Deniss Gorjunov <goryunov.deniska@list.ru>
2024-12-04 16:22:26 +0200

    create prod

---
+++ RouteManagementAPI/Models/Route.cs
@@ -0,0 +1,18 @@
+﻿// Models/Route.cs
+using System;
+
+namespace RouteManagementAPI.Models
+{
+    public class Route
+    {
+        public int Id { get; set; }
+        public string Source { get; set; }
+        public string Destination { get; set; }
+        public DateTime DepartureTime { get; set; }
+
+        // Три логических атрибута/измерения
+        public string Dimension1 { get; set; }
+        public string Dimension2 { get; set; }
+        public string Dimension3 { get; set; }
+    }
+}
